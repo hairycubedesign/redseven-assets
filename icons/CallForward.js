afc3213573723419ff7f79e56dddeacb06a6ec6e
Jim O'Brien <jim.obrien@redseven.tech>
2019-04-01 14:15:14 +0100

    added Call Forward icon

---
+++ icons/CallForward.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _createSvgIcon = _interopRequireDefault(require("./utils/createSvgIcon"));
+
+var _default = (0, _createSvgIcon.default)(_react.default.createElement(_react.default.Fragment, null, _react.default.createElement("polygon", {
+  fill: "#000000",
+  points: "16 12 21 7 16 2 16 5 13.9179688 5 11 5 11 9 16 9"
+}), _react.default.createElement("path", {
+  fill: "#000000",
+  d: "M19.8888889,15.8888889 C18.5055556,15.8888889 17.1722222,15.6666667 15.9222222,15.2555556 C15.5388889,15.1333333 15.1,15.2222222 14.7944444,15.5277778 L12.35,17.9777778 C9.2,16.3777778 6.63333333,13.8055556 5.02777778,10.6611111 L7.47222222,8.21111111 C7.77777778,7.90555556 7.86666667,7.46666667 7.74444444,7.08333333 C7.33333333,5.82777778 7.11111111,4.49444444 7.11111111,3.11111111 C7.11111111,2.49444444 6.61666667,2 6,2 L2.11111111,2 C1.49444444,2 1,2.49444444 1,3.11111111 C1,13.5444444 9.45555556,22 19.8888889,22 C20.5055556,22 21,21.5055556 21,20.8888889 L21,17 C21,16.3833333 20.5055556,15.8888889 19.8888889,15.8888889 Z"
+})), 'CallForward');
+
+
+exports.default = _default;
