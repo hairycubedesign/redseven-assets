1e82df90270feeb0a428650d2ca7ca2cfd90bc10
Jim O'Brien <jim.obrien@redseven.tech>
2019-04-01 13:39:25 +0100

    added Caller ID icon

---
+++ icons/CallerId.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _createSvgIcon = _interopRequireDefault(require("./utils/createSvgIcon"));
+
+var _default = (0, _createSvgIcon.default)(_react.default.createElement(_react.default.Fragment, null, _react.default.createElement("path", {
+  stroke: "#000000",
+  fill: "#ffffff",
+  d: "M21.1666667,3 L2.83333333,3 C2.41976895,3 2,3.50881085 2,4.22222222 L2,19.7777778 C2,20.4911891 2.41976895,21 2.83333333,21 L21.1666667,21 C21.5766583,21 21.9908333,20.4944773 21.9908335,19.7771885 L22,4.22222222 C22,3.50881085 21.580231,3 21.1666667,3 Z"
+}), _react.default.createElement("path", {
+  fill: "#000000",
+  d: "M13.9,17.2 L4,17.2 L4,16.35 C4,14.65 7.3,13.715 8.95,13.715 C10.6,13.715 13.9,14.65 13.9,16.35 L13.9,17.2 Z"
+}), _react.default.createElement("path", {
+  fill: "#000000",
+  d: "M8.95,7 C10.315375,7 11.425,8.14325 11.425,9.55 C11.425,10.961 10.315375,12.1 8.95,12.1 C7.584625,12.1 6.475,10.961 6.475,9.55 C6.475,8.14325 7.584625,7 8.95,7 Z"
+}), _react.default.createElement("path", {
+  fill: "#000000",
+  d: "M16.6625003,13.9888893 L18.1658337,13.9888893 L19.5500003,15.8777782 L17.7212503,17.7619449 C16.5250003,16.8363893 15.6312503,15.5188893 15.2187503,13.9888893 C15.0583337,13.3844449 14.966667,12.7563893 14.966667,12.1000004 C14.966667,11.4436116 15.0583337,10.815556 15.2233337,10.2111116 C15.6312503,8.68111156 16.5250003,7.36361156 17.7258337,6.438056 L19.5500003,8.32222267 L18.1658337,10.2111116 L16.6625003,10.2111116 C16.4608337,10.8013893 16.341667,11.4388893 16.341667,12.1000004 C16.341667,12.7611116 16.4562503,13.3986116 16.6625003,13.9888893 Z"
+})), 'CallerId');
+
+
+exports.default = _default;
