d88d7b1f395b4f4d4c006907b8421b734886ba0d
Jim O'Brien <jim.obrien@redseven.tech>
2019-04-01 16:23:53 +0100

    added FMFM and music on hold icons

---
+++ icons/FindMeFollowMe.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _createSvgIcon = _interopRequireDefault(require("./utils/createSvgIcon"));
+
+var _default = (0, _createSvgIcon.default)(_react.default.createElement(_react.default.Fragment, null, _react.default.createElement("path", {
+  fill: "",
+  transform: "translate(12.066284, 12.088379) scale(1, -1) translate(-12.066284, -12.088379) ",
+  d: "M19.1003418,9.16088867 L19.1003418,10.9767578 L13,10.9767578 L13,9.16088867 L11,9.16088867 L11,10.9767578 L5.03222656,10.9767578 L5.03222656,9.16088867 C5.11767578,9.16088867 3.03222656,9.16088867 3.03222656,9.16088867 L3.03222656,10.9767578 L3.03222656,12.9767578 L5.03222656,12.9767578 L11,12.9767578 L11,15.0158691 L13,15.0158691 L13,12.9767578 L19.1003418,12.9767578 L21.1003418,12.9767578 L21.1003418,10.9767578 L21.1003418,9.16088867 L19.1003418,9.16088867 Z"
+}), _react.default.createElement("circle", {
+  fill: "",
+  cx: "12",
+  cy: "5",
+  r: "3"
+}), _react.default.createElement("circle", {
+  fill: "",
+  cx: "4",
+  cy: "19",
+  r: "3"
+}), _react.default.createElement("circle", {
+  fill: "",
+  cx: "12",
+  cy: "19",
+  r: "3"
+}), _react.default.createElement("circle", {
+  fill: "",
+  cx: "20",
+  cy: "19",
+  r: "3"
+})), 'FindMeFollowMe');
+
+
+exports.default = _default;
---
+++ icons/MusicOnHold.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _createSvgIcon = _interopRequireDefault(require("./utils/createSvgIcon"));
+
+var _default = (0, _createSvgIcon.default)(_react.default.createElement(_react.default.Fragment, null, _react.default.createElement("polygon", {
+  fill: "",
+  d: "15 6 3 6 3 8 15 8"
+}), _react.default.createElement("polygon", {
+  fill: "",
+  d: "15 10 3 10 3 12 15 12"
+}), _react.default.createElement("polygon", {
+  fill: "",
+  d: "3 16 11 16 11 14 3 14"
+}), _react.default.createElement("path", {
+  fill: "",
+  d: "M17,6 L17,14.185 C16.685,14.07 16.355,14 16,14 C14.345,14 13,15.345 13,17 C13,18.655 14.345,20 16,20 C17.655,20 19,18.655 19,17 L19,8 L22,8 L22,6 L17,6 Z"
+})), 'MusicOnHold');
+
+
+exports.default = _default;
