ed16e98ccb6f959e39cac23de99281f0672c5f06
Jim O'Brien <jim.obrien@redseven.tech>
2019-04-02 11:29:55 +0100

    added extra actions buttons for Delete/Provision/Restrictions

---
+++ icons/actions/Delete.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _createSvgIcon = _interopRequireDefault(require("../utils/createSvgIcon"));
+
+var _default = (0, _createSvgIcon.default)(_react.default.createElement(_react.default.Fragment, null, _react.default.createElement("path", {
+  //bin outline
+  fill: "",
+  d: "M19.8854,5.125 C20.4024,5.125 20.8224,5.545 20.8224,6.063 L20.8224,6.531 C20.8224,6.79 20.6124,7 20.3534,7 L19.5724,7 L19.5724,20.125 C19.5724,21.16 18.7324,22 17.6974,22 L6.4474,22 C5.4114,22 4.5724,21.16 4.5724,20.125 L4.5724,7 L3.7914,7 C3.5324,7 3.3224,6.79 3.3224,6.531 L3.3224,6.063 C3.3224,5.545 3.7414,5.125 4.2594,5.125 L7.1664,5.125 L8.4954,2.91 C8.8344,2.346 9.4444,2 10.1024,2 L14.0414,2 C14.7004,2 15.3104,2.346 15.6494,2.91 L16.9774,5.125 L19.8854,5.125 Z"
+
+}), _react.default.createElement("path", {
+  //handle knockout
+  fill: "#ffffff",
+  d: "M17.6974,7 L6.4474,7 L6.4474,19.891 C6.4474,20.021 6.5514,20.125 6.6814,20.125 L17.4634,20.125 C17.5924,20.125 17.6974,20.021 17.6974,19.891 L17.6974,7 Z"
+
+}), _react.default.createElement("path", {
+  //bin knockout
+  fill: "#ffffff",
+  d: "M9.3524,5.125 L14.7914,5.125 L14.1094,3.989 C14.0674,3.918 13.9914,3.875 13.9084,3.875 L10.2354,3.875 C10.1534,3.875 10.0774,3.918 10.0344,3.989 L9.3524,5.125 Z"
+
+}), _react.default.createElement("path", {
+  //bin line left
+  fill: "",
+  d: "M14.7284,8.875 L13.7914,8.875 C13.5324,8.875 13.3224,9.085 13.3224,9.344 L13.3224,17.781 C13.3224,18.04 13.5324,18.25 13.7914,18.25 L14.7284,18.25 C14.9874,18.25 15.1974,18.04 15.1974,17.781 L15.1974,9.344 C15.1974,9.085 14.9874,8.875 14.7284,8.875 L14.7284,8.875 Z"
+
+}), _react.default.createElement("path", {
+  //bin line right
+  fill: "",
+  d: "M10.8224,9.344 L10.8224,17.781 C10.8224,18.04 10.6124,18.25 10.3534,18.25 L9.4164,18.25 C9.1574,18.25 8.9474,18.04 8.9474,17.781 L8.9474,9.344 C8.9474,9.085 9.1574,8.875 9.4164,8.875 L10.3534,8.875 C10.6124,8.875 10.8224,9.085 10.8224,9.344 L10.8224,9.344 Z"
+
+})), 'Delete');
+
+
+exports.default = _default;
---
+++ icons/actions/Provision.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _createSvgIcon = _interopRequireDefault(require("../utils/createSvgIcon"));
+
+var _default = (0, _createSvgIcon.default)(_react.default.createElement(_react.default.Fragment, null, _react.default.createElement("path", {
+  //phone handset
+  fill: "",
+  d: "M19.8888889,15.8888889 C18.5055556,15.8888889 17.1722222,15.6666667 15.9222222,15.2555556 C15.5388889,15.1333333 15.1,15.2222222 14.7944444,15.5277778 L12.35,17.9777778 C9.2,16.3777778 6.63333333,13.8055556 5.02777778,10.6611111 L7.47222222,8.21111111 C7.77777778,7.90555556 7.86666667,7.46666667 7.74444444,7.08333333 C7.33333333,5.82777778 7.11111111,4.49444444 7.11111111,3.11111111 C7.11111111,2.49444444 6.61666667,2 6,2 L2.11111111,2 C1.49444444,2 1,2.49444444 1,3.11111111 C1,13.5444444 9.45555556,22 19.8888889,22 C20.5055556,22 21,21.5055556 21,20.8888889 L21,17 C21,16.3833333 20.5055556,15.8888889 19.8888889,15.8888889 Z"
+
+}), _react.default.createElement("path", {
+  //key
+  fill: "",
+  d: "M15.825,4 C15.412,2.835 14.305,2 13,2 C11.343,2 10,3.343 10,5 C10,6.657 11.343,8 13,8 C14.305,8 15.412,7.165 15.825,6 L18,6 L18,8 L20,8 L20,6 L21,6 L21,4 L15.825,4 Z"
+
+}), _react.default.createElement("path", {
+  //key knockout
+  fill: "#ffffff",
+  d: "M13,6 C12.447,6 12,5.553 12,5 C12,4.447 12.447,4 13,4 C13.553,4 14,4.447 14,5 C14,5.553 13.553,6 13,6 L13,6 Z"
+
+})), 'Provision');
+
+
+exports.default = _default;
