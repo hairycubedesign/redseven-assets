2bdf27e6d79c501613b7c90ba057f3aa1e8a2139
Jim O'Brien <jim.obrien@redseven.tech>
2019-04-02 10:25:15 +0100

    improved SIP phone paths

--- icons/SipPhone.js
+++ icons/SipPhone.js
@@ -12,60 +12,115 @@ var _react = _interopRequireDefault(require("react"));
 var _createSvgIcon = _interopRequireDefault(require("./utils/createSvgIcon"));
 
 var _default = (0, _createSvgIcon.default)(_react.default.createElement(_react.default.Fragment, null, _react.default.createElement("path", {
+  //base left edge
   fill: "",
   d: "M2.3,19.875 C2.3,20.7144241 2.59708862,21 3.30698853,21 C3.30698853,21 3.6451945,21 4.32160645,21 L4.32160645,3 C3.71453548,3 3.411,3 3.411,3 C2.63026123,3 2.3,3.56538391 2.3,4.125 C2.3,4.125 2.3,19.875 2.3,19.875 Z"
+
 }), _react.default.createElement("polygon", {
+  //receiver
   fill: "",
   points: "5.2 22 9.2 22 9.2 2 5.2 2"
+
 }), _react.default.createElement("path", {
+  //base outline
   fill: "",
   d: "M11.5,3 L20.5,3 C21.3284271,3 22,3.67157288 22,4.5 L22,19.5 C22,20.3284271 21.3284271,21 20.5,21 L11.5,21 C10.6715729,21 10,20.3284271 10,19.5 L10,4.5 C10,3.67157288 10.6715729,3 11.5,3 Z"
+
+}), _react.default.createElement("path", {
+  //base screen knockout
+  fill: "#ffffff",
+  d: "M11.5,4.3 C11.3895431,4.3 11.3,4.38954305 11.3,4.5 C11.3,7.98781076 11.3,10.6483739 11.3,12.4816895 C11.3,12.4816895 20.7,12.5081787 20.7,12.5081787 C20.7,10.7285834 20.7,8.05919054 20.7,4.5 C20.7,4.38954305 20.6104569,4.3 20.5,4.3 L11.5,4.3 Z"
+
+}), _react.default.createElement("rect", {
+  //top left keypad button
+  fill: "#ffffff",
+  x: "11.3",
+  y: "13.5",
+  width: "2.55",
+  height: "1.35"
+
+}), _react.default.createElement("rect", {
+  //top middle keypad button
+  fill: "#ffffff",
+  x: "14.725",
+  y: "13.5",
+  width: "2.55",
+  height: "1.35"
+
+}), _react.default.createElement("rect", {
+  //top right keypad button
+  fill: "#ffffff",
+  x: "18.15",
+  y: "13.5",
+  width: "2.55",
+  height: "1.35"
+
+}), _react.default.createElement("rect", {
+  //centre left keypad button
+  fill: "#ffffff",
+  x: "11.3",
+  y: "18.35",
+  width: "2.55",
+  height: "1.35"
+
+}), _react.default.createElement("rect", {
+  //centre middle keypad button
+  fill: "#ffffff",
+  x: "14.725",
+  y: "18.35",
+  width: "2.55",
+  height: "1.35"
+
+}), _react.default.createElement("rect", {
+  //centre right keypad button
+  fill: "#ffffff",
+  x: "18.15",
+  y: "18.35",
+  width: "2.55",
+  height: "1.35"
+
+}), _react.default.createElement("rect", {
+  //bottom left keypad button
+  fill: "#ffffff",
+  x: "11.3",
+  y: "15.925",
+  width: "2.55",
+  height: "1.35"
+
+}), _react.default.createElement("rect", {
+  //bottom middle keypad button
+  fill: "#ffffff",
+  x: "14.725",
+  y: "15.925",
+  width: "2.55",
+  height: "1.35"
+
+}), _react.default.createElement("rect", {
+  //bottom right keypad button
+  fill: "#ffffff",
+  x: "18.15",
+  y: "15.925",
+  width: "2.55",
+  height: "1.35"
+
 }), _react.default.createElement("polygon", {
-  fill: "",
-  points: "11.5 4.5 11.5 19.5 20.5 19.5 20.5 4.5"
-}), _react.default.createElement("rect", {
-  fill: "",
-  x: "10.5",
-  y: "12.5",
-  width: "11",
-  height: "1"
-}), _react.default.createElement("rect", {
-  fill: "",
-  x: "10.5",
-  y: "17.1666667",
-  width: "11",
-  height: "1"
-}), _react.default.createElement("rect", {
-  fill: "",
-  x: "10.5",
-  y: "14.8333333",
-  width: "11",
-  height: "1"
-}), _react.default.createElement("rect", {
-  fill: "",
-  transform: "translate(17.666667, 16.500000) rotate(-90.000000) translate(-17.666667, -16.500000)",
-  x: "13.6666667",
-  y: "16",
-  width: "8",
-  height: "1"
-}), _react.default.createElement("rect", {
-  fill: "",
-  transform: "translate(14.333333, 16.500000) rotate(-90.000000) translate(-14.333333, -16.500000)",
-  x: "10.3333333",
-  y: "16",
-  width: "8",
-  height: "1"
-}), _react.default.createElement("polygon", {
+  // screen label "S"
   fill: "",
   points: "14.3 8.1 12.9666667 8.1 12.9666667 7.3 14.3 7.3 14.3 6.5 12.3 6.5 12.3 8.9 13.6333333 8.9 13.6333333 9.7 12.3 9.7 12.3 10.5 14.3 10.5"
+
 }), _react.default.createElement("polygon", {
+  // screen label "I"
   fill: "",
   points: "16.2 6.5 15.4 6.5 15.4 10.5 16.2 10.5"
+
 }), _react.default.createElement("polygon", {
+  // screen label "P"
   fill: "",
   points: "17.3 6.5 17.3 10.5 18.066667 10.5 18.066667 8.9 19.5 8.9 19.5 6.5"
+
 }), _react.default.createElement("polygon", {
-  fill: "",
+  // screen label "P" knockout
+  fill: "#ffffff",
   points: "18.8 8.2 18 8.2 18 7.4 18.8 7.4"
 })), 'SipPhone');
 
